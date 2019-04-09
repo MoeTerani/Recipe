84b582fdbe700eae726cabc53e950d6b930e12ea
MOE <moe.terani@gmail.com>
2019-04-09 10:12:29 +0200

    Webpack configured - dev -build

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+
+import x from "./test";
+
+console.log(`I imported ${x} from another module`);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js"
+  }
+};
