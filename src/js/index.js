2e37c6ddeef77af2c7f6bb05968d06d430a8f98d
MOE <moe.terani@gmail.com>
2019-04-10 09:58:17 +0200

    Search , render UI , clear

--- src/js/index.js
+++ src/js/index.js
@@ -12,23 +12,51 @@ console.log(`Using imported vriables ${add(5, 5)}`);
 // https://www.food2fork.com/api/search
 // https://www.food2fork.com/api/get 
 
-// APIKEY: 302483c930b58460ecbba917f0ee3ce8
-
+// APIKEY 1: 302483c930b58460ecbba917f0ee3ce8
+// APIKEY 2: a6d4a138f18cee95ac4b8fdf5fc4dfdd
 
 // https://www.food2fork.com/api/search?key=302483c930b58460ecbba917f0ee3ce8&q=chicken%20breast&page=2 
 
-import axios from 'axios';
+import Search from './models/Search'
+import * as searchView from './views/SearchView'
+import { elements } from './views/base';
+/** Global state of the app
+ * -Search object
+ * -Current recipe object
+ * -shopping list
+ * -liked recipe
+ */
+const state = {
 
-async function getResult(query) {
-    const key = '302483c930b58460ecbba917f0ee3ce8';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query} 
-    `);
-        const recipeSearch = res.data.recipes;
-        console.log(recipeSearch[0]);
-    }
-    catch (error) {
-        alert(error);
+};
+
+const controlSearch = async () => {
+    //Get the query from the view
+    const query = searchView.getInput();
+    console.log(query);
+
+
+    if (query) {
+        // 2: new search object and add it to state
+        state.search = new Search(query);
+
+        // 3: prepare UI for result
+        searchView.clearInput();
+        searchView.clearResList();
+        // 4: Search for recipes
+        await state.search.getResults();
+
+        // 5: surrender result in UI
+        searchView.renderResults(state.search.result);
+
+
     }
 }
-getResult('pizza');
+
+elements.searchForm.addEventListener('submit', e => {
+    // prevent browser from refreshing when search button is pressed.
+    e.preventDefault();
+    controlSearch();
+});
+
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,23 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async  getResults() {
+        const key = '302483c930b58460ecbba917f0ee3ce8';
+        const key2 = 'a6d4a138f18cee95ac4b8fdf5fc4dfdd';
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key2}&q=${this.query} 
+        `);
+            this.result = res.data.recipes;
+        }
+        catch (error) {
+            alert(error);
+        }
+    }
+}
+
+
