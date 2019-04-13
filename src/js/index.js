ce3cf16b434fe06704772378b4280b83ad9dbc08
MOE <moe.terani@gmail.com>
2019-04-13 09:54:28 +0200

    Added AJAX loader spinner

--- src/js/index.js
+++ src/js/index.js
@@ -10,53 +10,49 @@ console.log(`Using imported vriables ${add(5, 5)}`);
 */
 
 // https://www.food2fork.com/api/search
-// https://www.food2fork.com/api/get 
+// https://www.food2fork.com/api/get
 
 // APIKEY 1: 302483c930b58460ecbba917f0ee3ce8
 // APIKEY 2: a6d4a138f18cee95ac4b8fdf5fc4dfdd
 
-// https://www.food2fork.com/api/search?key=302483c930b58460ecbba917f0ee3ce8&q=chicken%20breast&page=2 
+// https://www.food2fork.com/api/search?key=302483c930b58460ecbba917f0ee3ce8&q=chicken%20breast&page=2
 
-import Search from './models/Search'
-import * as searchView from './views/SearchView'
-import { elements } from './views/base';
+import Search from './models/Search';
+import * as searchView from './views/SearchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
 /** Global state of the app
  * -Search object
  * -Current recipe object
  * -shopping list
  * -liked recipe
  */
-const state = {
-
-};
+const state = {};
 
 const controlSearch = async () => {
-    //Get the query from the view
-    const query = searchView.getInput();
-    console.log(query);
+  //Get the query from the view
+  const query = searchView.getInput();
+  console.log(query);
 
+  if (query) {
+    // 2: new search object and add it to state
+    state.search = new Search(query);
 
-    if (query) {
-        // 2: new search object and add it to state
-        state.search = new Search(query);
+    // 3: prepare UI for result
+    searchView.clearInput();
+    searchView.clearResList();
+    renderLoader(elements.searchRes);
+    // 4: Search for recipes
+    await state.search.getResults();
 
-        // 3: prepare UI for result
-        searchView.clearInput();
-        searchView.clearResList();
-        // 4: Search for recipes
-        await state.search.getResults();
-
-        // 5: surrender result in UI
-        searchView.renderResults(state.search.result);
-
-
-    }
-}
+    // 5: surrender result in UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
 
 elements.searchForm.addEventListener('submit', e => {
-    // prevent browser from refreshing when search button is pressed.
-    e.preventDefault();
-    controlSearch();
+  // prevent browser from refreshing when search button is pressed.
+  e.preventDefault();
+  controlSearch();
 });
-
-
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,30 @@
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    searchResList: document.querySelector('.results__list')
-}
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  searchRes: document.querySelector('.results'),
+  searchResList: document.querySelector('.results__list')
+};
+
+export const elementStrings = {
+  loader: 'loader'
+};
+
+// spinner is here because of reusability and not in the serachView file.
+export const renderLoader = parent => {
+  const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+  parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) {
+    // deleting the element completely from the DOM
+    loader.parentElement.removeChild(loader);
+  }
+};
